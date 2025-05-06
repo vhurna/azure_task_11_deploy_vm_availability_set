160bb882c4cb315e222bb3987568b157ff45089c
mira <mkyshchak@gmail.com>
2025-05-07 00:01:34 +0300

    availability-set-vm

--- task.ps1
+++ task.ps1
@@ -1,39 +1,84 @@
-$location = "uksouth"
-$resourceGroupName = "mate-azure-task-11"
-$networkSecurityGroupName = "defaultnsg"
-$virtualNetworkName = "vnet"
-$subnetName = "default"
-$vnetAddressPrefix = "10.0.0.0/16"
-$subnetAddressPrefix = "10.0.0.0/24"
-$sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
-$vmName = "matebox"
-$vmImage = "Ubuntu2204"
-$vmSize = "Standard_B1s"
-$availabilitySetName = "mateavalset"
+# Parameters
+$location                  = "uksouth"
+$resourceGroupName         = "mate-azure-task-11"
+$networkSecurityGroupName  = "defaultnsg"
+$virtualNetworkName        = "vnet"
+$subnetName                = "default"
+$vnetAddressPrefix         = "10.0.0.0/16"
+$subnetAddressPrefix       = "10.0.0.0/24"
+$sshKeyName                = "linuxboxsshkey"
+$sshKeyPublicKey           = Get-Content "~/.ssh/id_rsa.pub"
+$vmName                    = "matebox"
+$vmImage                   = "Ubuntu2204"
+$vmSize                    = "Standard_B1s"
+$availabilitySetName       = "mateavalset"
 
-Write-Host "Creating a resource group $resourceGroupName ..."
+# Create resource group
+Write-Host "Creating resource group $resourceGroupName..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-Write-Host "Creating a network security group $networkSecurityGroupName ..."
-$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow;
-$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
-New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
+# Create network security group
+Write-Host "Creating network security group $networkSecurityGroupName..."
+$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH -Protocol Tcp -Direction Inbound -Priority 1001 `
+    -SourceAddressPrefix * -SourcePortRange * `
+    -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow
+$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP -Protocol Tcp -Direction Inbound -Priority 1002 `
+    -SourceAddressPrefix * -SourcePortRange * `
+    -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow
 
-$subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
-
-New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
-
-for (($zone = 1); ($zone -le 2); ($zone++) ) {
-    New-AzVm `
+$nsg = New-AzNetworkSecurityGroup `
+    -Name $networkSecurityGroupName `
     -ResourceGroupName $resourceGroupName `
-    -Name "$vmName-$zone" `
     -Location $location `
-    -image $vmImage `
-    -size $vmSize `
-    -SubnetName $subnetName `
-    -VirtualNetworkName $virtualNetworkName `
-    -SecurityGroupName $networkSecurityGroupName `
-    -SshKeyName $sshKeyName -Zone $zone
+    -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
+
+# Create virtual network and subnet
+Write-Host "Creating virtual network $virtualNetworkName with subnet $subnetName..."
+$subnet = New-AzVirtualNetworkSubnetConfig `
+    -Name $subnetName `
+    -AddressPrefix $subnetAddressPrefix `
+    -NetworkSecurityGroup $nsg
+
+$vnet = New-AzVirtualNetwork `
+    -Name $virtualNetworkName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -AddressPrefix $vnetAddressPrefix `
+    -Subnet $subnet
+
+# Import SSH key
+Write-Host "Importing SSH public key $sshKeyName..."
+New-AzSshKey `
+    -Name $sshKeyName `
+    -ResourceGroupName $resourceGroupName `
+    -PublicKey $sshKeyPublicKey
+
+# Create availability set
+Write-Host "Creating availability set $availabilitySetName..."
+$avSet = New-AzAvailabilitySet `
+    -ResourceGroupName $resourceGroupName `
+    -Name $availabilitySetName `
+    -Location $location `
+    -PlatformFaultDomainCount 2 `
+    -PlatformUpdateDomainCount 2 `
+    -Sku Aligned
+
+# Deploy two VMs in the availability set
+for ($i = 1; $i -le 2; $i++) {
+    $vmNameWithNumber = "$vmName-$i"
+    Write-Host "Creating VM $vmNameWithNumber in availability set..."
+    
+    New-AzVm `
+        -ResourceGroupName $resourceGroupName `
+        -Name $vmNameWithNumber `
+        -Location $location `
+        -Image $vmImage `
+        -Size $vmSize `
+        -VirtualNetworkName $virtualNetworkName `
+        -SubnetName $subnetName `
+        -SecurityGroupName $networkSecurityGroupName `
+        -SshKeyName $sshKeyName `
+        -AvailabilitySetName $availabilitySetName
 }
+
+Write-Host "`n✅ Successfully deployed two VMs ($vmName-1, $vmName-2) in availability set '$availabilitySetName'."
